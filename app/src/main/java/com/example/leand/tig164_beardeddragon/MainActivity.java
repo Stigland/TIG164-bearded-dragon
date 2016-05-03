264b6e8b52cdc710110933628402cc7a6738340c
unknown <leanderjohannes@gmail.com>
2016-05-03 22:43:53 +0200

    Added CalendarActivity, with functionality

--- app/src/main/java/com/example/leand/tig164_beardeddragon/MainActivity.java
+++ app/src/main/java/com/example/leand/tig164_beardeddragon/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.leand.tig164_beardeddragon;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button OpenCalendarBtn;
+    private Button OpenContactsBtn;
+    private Button OpenCheckInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OpenCalendarBtn   = (Button) findViewById(R.id.menu_OpenCalendar_Btn);
+        OpenContactsBtn   = (Button) findViewById(R.id.menu_OpenContacts_Btn);
+        OpenCheckInBtn    = (Button) findViewById(R.id.menu_OpenCheckIn_Btn);
+
+        //Open Calendar
+        View.OnClickListener openCalendarOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getBaseContext(), CalendarActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        OpenCalendarBtn.setOnClickListener(openCalendarOnClickListener);
+
+        //Open Contacts
+        View.OnClickListener openContactsOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
+        };
+        OpenContactsBtn.setOnClickListener(openContactsOnClickListener);
+
+        //Open Check In
+        View.OnClickListener openCheckInOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
+        };
+        OpenCheckInBtn.setOnClickListener(openCheckInOnClickListener);
     }
 }
