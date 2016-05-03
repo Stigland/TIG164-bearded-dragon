938a3cb07f7b8485e0da50a1ea6a3f84a8af559e
unknown <leanderjohannes@gmail.com>
2016-05-03 22:50:38 +0200

    Added CheckInActivity, with function

---
+++ app/src/main/java/com/example/leand/tig164_beardeddragon/CheckInActivity.java
@@ -0,0 +1,15 @@
+package com.example.leand.tig164_beardeddragon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by leand on 2016-05-03.
+ */
+public class CheckInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_in);
+    }
+}
