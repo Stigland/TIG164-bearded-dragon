3b98438e9fc15286baf429d3a50573ac0526d3e5
unknown <leanderjohannes@gmail.com>
2016-05-04 13:29:58 +0200

    Added basic calendar

--- app/src/main/java/com/example/leand/tig164_beardeddragon/MainActivity.java
+++ app/src/main/java/com/example/leand/tig164_beardeddragon/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.leand.tig164_beardeddragon;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
