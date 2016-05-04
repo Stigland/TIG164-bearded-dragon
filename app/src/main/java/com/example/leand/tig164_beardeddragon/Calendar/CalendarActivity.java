b7f851dd3ebd9da1ed2c029b38c62fa7aea1f283
unknown <leanderjohannes@gmail.com>
2016-05-04 13:44:44 +0200

    Created initiateCalendar method

--- app/src/main/java/com/example/leand/tig164_beardeddragon/Calendar/CalendarActivity.java
+++ app/src/main/java/com/example/leand/tig164_beardeddragon/Calendar/CalendarActivity.java
@@ -1,4 +1,4 @@
-package com.example.leand.tig164_beardeddragon;
+package com.example.leand.tig164_beardeddragon.Calendar;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
@@ -20,11 +20,15 @@ public class CalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
+        initiateCalendar();
+    }
+
+    private void initiateCalendar() {
         CaldroidFragment caldroidFragment = new CaldroidFragment();
         Bundle args = new Bundle();
         Calendar cal = Calendar.getInstance();
 
-        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH)+1);
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
         args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
         caldroidFragment.setArguments(args);
 
--- app/src/main/java/com/example/leand/tig164_beardeddragon/MainActivity.java
+++ app/src/main/java/com/example/leand/tig164_beardeddragon/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.leand.tig164_beardeddragon.Calendar.CalendarActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button OpenCalendarBtn;
