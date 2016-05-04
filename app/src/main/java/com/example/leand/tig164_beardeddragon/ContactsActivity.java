827553a4c5f3e3f8922d227ba25c7242f7c33b94
Stigland <edvin.stigland@hotmail.com>
2016-05-04 14:46:34 +0200

    First commit

--- app/src/main/java/com/example/leand/tig164_beardeddragon/ContactsActivity.java
+++ app/src/main/java/com/example/leand/tig164_beardeddragon/ContactsActivity.java
@@ -2,6 +2,13 @@ package com.example.leand.tig164_beardeddragon;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 /**
  * Created by leand on 2016-05-03.
@@ -11,5 +18,65 @@ public class ContactsActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+    }
+}
+
+public class Contact {
+
+    private int contactID;
+    private String name;
+    private String email;
+    private String messengerURI;
+    private String phone;
+
+    public Contact(int contactID, String name, String email, String messengerURI, String phone){
+
+        super();
+        this.contactID = contactID;
+        this.name = name;
+        this.email = email;
+        this.messengerURI = messengerURI;
+        this.phone = phone;
+
+    }
+
+    public int getContactID() {
+        return contactID;
+    }
+
+    public void setContactID(int contactID) {
+        this.contactID = contactID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessengerURI() {
+        return messengerURI;
+    }
+
+    public void setMessengerURI(String messengerURI) {
+        this.messengerURI = messengerURI;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
