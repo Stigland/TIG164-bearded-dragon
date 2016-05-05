a9cc9bb00b062dbf0f49ca56186a76873ee14d6d
unknown <leanderjohannes@gmail.com>
2016-05-05 21:09:39 +0200

    Created calendar color descriptions

--- app/src/main/java/com/example/leand/tig164_beardeddragon/Calendar/CalendarActivity.java
+++ app/src/main/java/com/example/leand/tig164_beardeddragon/Calendar/CalendarActivity.java
@@ -1,5 +1,8 @@
 package com.example.leand.tig164_beardeddragon.Calendar;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +11,7 @@ import com.example.leand.tig164_beardeddragon.R;
 import com.roomorama.caldroid.CaldroidFragment;
 
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by leand on 2016-05-03.
@@ -35,5 +39,14 @@ public class CalendarActivity extends AppCompatActivity {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.calendar1, caldroidFragment);
         ft.commit();
+
+        Calendar greenCal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -7);
+        Date greenDate = cal.getTime();
+
+        ColorDrawable green = new ColorDrawable(Color.GREEN);
+        caldroidFragment.setBackgroundDrawableForDate(green, greenDate);
+
+        caldroidFragment.refreshView();
     }
 }
