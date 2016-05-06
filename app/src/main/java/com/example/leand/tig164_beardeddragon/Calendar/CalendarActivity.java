1d09f67b80f92abe84220264c1cffafc89409e4a
unknown <leanderjohannes@gmail.com>
2016-05-06 15:56:24 +0200

    Dates can be colored

--- app/src/main/java/com/example/leand/tig164_beardeddragon/Calendar/CalendarActivity.java
+++ app/src/main/java/com/example/leand/tig164_beardeddragon/Calendar/CalendarActivity.java
@@ -1,14 +1,19 @@
 package com.example.leand.tig164_beardeddragon.Calendar;
 
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.leand.tig164_beardeddragon.R;
 import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -19,6 +24,12 @@ import java.util.Date;
 
 public class CalendarActivity extends AppCompatActivity {
 
+    private Calendar cal = Calendar.getInstance();
+    ColorDrawable lightBlue = new ColorDrawable(getResources().getColor(R.color.lightBlue));
+    ColorDrawable lightGreen = new ColorDrawable(getResources().getColor(R.color.lightGreen));
+    ColorDrawable yellow = new ColorDrawable(getResources().getColor(R.color.yellow));
+    ColorDrawable orange = new ColorDrawable(getResources().getColor(R.color.orange));
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +39,8 @@ public class CalendarActivity extends AppCompatActivity {
     }
 
     private void initiateCalendar() {
-        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        final CaldroidFragment caldroidFragment = new CaldroidFragment();
         Bundle args = new Bundle();
-        Calendar cal = Calendar.getInstance();
 
         args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
         args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
@@ -40,13 +50,19 @@ public class CalendarActivity extends AppCompatActivity {
         ft.replace(R.id.calendar1, caldroidFragment);
         ft.commit();
 
-        Calendar greenCal = Calendar.getInstance();
-        cal.add(Calendar.DATE, -7);
-        Date greenDate = cal.getTime();
+        final CaldroidListener listener = new CaldroidListener() {
 
-        ColorDrawable green = new ColorDrawable(Color.GREEN);
-        caldroidFragment.setBackgroundDrawableForDate(green, greenDate);
+            @Override
+            public void onSelectDate(Date date, View view) {
+                final Date blueDate = cal.getTime();
+                ColorDrawable blue = new ColorDrawable(Color.BLUE);
+                caldroidFragment.setBackgroundDrawableForDate(blue, date);
 
-        caldroidFragment.refreshView();
+                caldroidFragment.refreshView();
+            }
+        };
+
+        caldroidFragment.setCaldroidListener(listener);
+
     }
 }
