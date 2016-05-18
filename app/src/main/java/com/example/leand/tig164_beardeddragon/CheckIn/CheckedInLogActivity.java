6a3d29650b5c732000b79afb6182dc8298c299c6
Anton <wahlberg.anton@gmail.com>
2016-05-18 10:38:19 +0200

    Small cleansing in checkIn

--- app/src/main/java/com/example/leand/tig164_beardeddragon/CheckIn/CheckedInLogActivity.java
+++ app/src/main/java/com/example/leand/tig164_beardeddragon/CheckIn/CheckedInLogActivity.java
@@ -1,8 +1,10 @@
 package com.example.leand.tig164_beardeddragon.CheckIn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import com.example.leand.tig164_beardeddragon.R;
 
@@ -14,21 +16,24 @@ public class CheckedInLogActivity extends AppCompatActivity{
     //private Button backToCheckOut;
     private TextView logTitle;
     private TextView logTV;
+    private Button backBtn;
 
-    /*Sets functionality for potential back button
     View.OnClickListener backToMainOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent i = new Intent(getBaseContext(), CheckedInActivity.class);
             startActivity(i);
-            //finish();
+            finish();
         }
-    };*/
+    };
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
 
+        backBtn = (Button) findViewById(R.id.back_btn);
+        backBtn.setOnClickListener(backToMainOnClickListener);
+        assert backBtn != null;
         logTV = (TextView) findViewById(R.id.schedule_log_tv);
         logTV.setText(CheckInTime.getLogString());
     }
